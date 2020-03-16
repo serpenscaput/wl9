649f4268bc0a520215c4093bdf044c58aef1154e
serpenscaput <serpenscaput@users.noreply.github.com>
2020-03-16 18:28:33 +0100

    quazampium chronicles

---
+++ Quazampium Chronicles 5.3/Quazampium Chronicles 5.3 - C.C
@@ -0,0 +1,21 @@
+Welcome To The Quazampium Chronicles
+       v 5.3 "I Love Me"
+
+Note:  This game does not express the views of any DasFooz Gamez people,
+       it was made as a joke!!! 
+
+
+----------Give Credit Where Credit Is Due----------
+
+Cedric B   -  Funnies Manager
+Wilhelm    -  Master Of The Wall
+Squeak     -  Master Of Operations
+Tao        -  Sits Quietly And Watches Us Work
+
+----------|||||||||||||||||||||||||||||||----------
+
+Please Read The Included README.TXT file, thanx!
+
+www.geocities.com/dasfooz
+
+
